35ccf82f484ffaf236b7d80ed0e8e99524c6740d
Collin Hodgson <Collinhodgs@gmail.com>
2022-02-08 21:11:22 -0700

    Added user enter prompts

--- InvoiceApp/Program.cs
+++ InvoiceApp/Program.cs
@@ -6,7 +6,22 @@ namespace InvoiceApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string partNumber;
+            string description;
+            int quantity;
+            decimal price;
+
+            Console.Write("Please enter the part number:");
+            partNumber = Console.ReadLine();
+
+            Console.Write("Please enter the part description:");
+            description = Console.ReadLine();
+
+            Console.Write("Please enter the quantity of items:");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the price of the item:");
+            price = Convert.ToDecimal(Console.ReadLine());
         }
     }
 }
